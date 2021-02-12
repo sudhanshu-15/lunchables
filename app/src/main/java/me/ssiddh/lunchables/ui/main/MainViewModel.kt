c76404502fa51345b2b32ce10bf19c906d20d2db
Sudhanshu Siddh <sudhanshu@ridecell.com>
2021-02-11 22:11:25 -0800

    Update viewmodel to take in repository as a dependency

--- app/src/main/java/me/ssiddh/lunchables/ui/main/MainViewModel.kt
+++ app/src/main/java/me/ssiddh/lunchables/ui/main/MainViewModel.kt
@@ -1,11 +1,19 @@
 package me.ssiddh.lunchables.ui.main
 
+import android.location.Location
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+import me.ssiddh.lunchables.data.repository.RestaurantSearchRepository
+import me.ssiddh.lunchables.network.GooglePlacesApiService
 import me.ssiddh.lunchables.utils.UIStates
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    private val restaurantSearchRepository: RestaurantSearchRepository
+) : ViewModel() {
 
     private val _uiState = MutableLiveData<UIStates>(UIStates.MapUIState)
 
@@ -17,6 +25,14 @@ class MainViewModel : ViewModel() {
             UIStates.ListUIState -> _uiState.value = UIStates.MapUIState
             UIStates.MapUIState -> _uiState.value = UIStates.ListUIState
             else -> UIStates.MapUIState
+        }
+    }
+
+    fun getNearByRestaurants(location: Location, searchQueryText: String? = null) {
+        viewModelScope.launch {
+            val searchResults =
+                restaurantSearchRepository.getNearByRestaurants(location, searchQueryText)
+            Log.d("SSLOG", "Search results ${searchResults.results.size}")
         }
     }
 }
