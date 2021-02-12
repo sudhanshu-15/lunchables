22235a04f29e973e0be74d8dcce5896fe82320b9
Sudhanshu Siddh <sudhanshu@ridecell.com>
2021-02-11 22:07:15 -0800

    Add api service for search api

---
+++ app/src/main/java/me/ssiddh/lunchables/network/GooglePlacesApiService.kt
@@ -0,0 +1,16 @@
+package me.ssiddh.lunchables.network
+
+import me.ssiddh.lunchables.data.models.SearchResults
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface GooglePlacesApiService {
+
+    @GET("nearbysearch/json")
+    suspend fun getNearByPlaces(
+        @Query("location") location: String,
+        @Query("radius") radius: Int = 1500,
+        @Query("type") type: String = "restaurant",
+        @Query("keyword") keyword: String?
+    ): SearchResults
+}
